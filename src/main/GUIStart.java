aa7d1b02004b3823b4a926e9e8dd014a07581355
DarkMlrage <92674008+DarkMlrage@users.noreply.github.com>
2022-11-23 18:27:25 +0200

    Starting GUI for курсова

---
+++ src/main/GUIStart.java
@@ -0,0 +1,8 @@
+package main;
+
+public class GUIStart {
+
+    public static void main(final String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,37 @@
 package main;
 
-import main.logger.Log;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
 import main.menu.MainMenu;
-import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Help");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
     public static void main(String[] args) {
-        PropertyConfigurator.configure("log4j.properties");
-
-        Scanner scanner = new Scanner(System.in);
-
-        MainMenu mainMenu = new MainMenu();
-        while (true) {
-            System.out.println("  __ Main Menu __");
-            System.out.println("Input your command: ");
-            List<String> command = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
-            mainMenu.execute(command);
-        }
+        launch(args);
+//        PropertyConfigurator.configure("log4j.properties");
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        MainMenu mainMenu = new MainMenu();
+//        while (true) {
+//            System.out.println("  __ Main Menu __");
+//            System.out.println("Input your command: ");
+//            List<String> command = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
+//            mainMenu.execute(command);
+//        }
     }
 }
