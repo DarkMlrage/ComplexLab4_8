d8668c7a9553bee54769640af77c97731a36645c
DarkMlrage <92674008+DarkMlrage@users.noreply.github.com>
2022-11-25 01:12:49 +0200

    -

--- src/gui/AppMainMenu.java
+++ src/gui/AppMainMenu.java
@@ -7,16 +7,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.menu.MainMenu;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class AppMainMenu extends Application {
-
+    private Stage primaryStage;
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainMenu.fxml")));
-        primaryStage.setTitle("Help");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Help");
+        initRootLayout();
+    }
+    public void initRootLayout(){
+        try{
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainMenu.fxml")));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
     public static void main(String[] args) {
         launch(args);
