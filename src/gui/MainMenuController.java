da82af526cdbc63cfd1afba6e6df21365cb5b132
DarkMlrage <92674008+DarkMlrage@users.noreply.github.com>
2022-11-25 20:10:37 +0200

    -

--- src/gui/MainMenuController.java
+++ src/gui/MainMenuController.java
@@ -1,18 +1,24 @@
 package gui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import main.bouquets.Bouquet;
 import main.order.Order;
+import main.utils.BouquetUtils;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
     protected Order order = new Order();
     protected Bouquet bouquet;
     private Stage stage;
@@ -44,8 +50,21 @@ public class MainMenuController {
     }
 
 
+    @FXML
+    private ChoiceBox<String> flowerChoiceBox;
+    private String[] flowers = {"redrose", "whiterose", "yellowtulip", "blacktulip"};
+    public void addFlowersButton(ActionEvent e) throws IOException {
+        AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("addFlowers.fxml")))));
+    }
+    public void addFlowers(ActionEvent e){
+        BouquetUtils.addFlowersToBouquet(bouquet, flowerChoiceBox.getValue(), );
+    }
+    public void backToBouquet(ActionEvent e) throws IOException {
+        AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("bouquetMenu.fxml")))));
+    }
 
-    public void addFlowers(ActionEvent e) throws IOException {
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        flowerChoiceBox.getItems().addAll(flowers);
     }
 }
