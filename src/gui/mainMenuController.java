5b9d82b9d4d4bad10b1a906d38f6f06be49c0df3
DarkMlrage <vovabuck@gmail.com>
2022-11-25 13:26:05 +0200

    main menu primitive structure done

--- src/gui/mainMenuController.java
+++ src/gui/mainMenuController.java
@@ -9,7 +9,7 @@ import main.order.Order;
 import java.io.IOException;
 import java.util.Objects;
 
-public class mainController {
+public class mainMenuController {
     protected Order order = new Order();
     public void addBouquet(ActionEvent e) throws IOException {
         Bouquet obj = new Bouquet();
@@ -19,18 +19,20 @@ public class mainController {
     public void deleteBouquet(ActionEvent e) throws IOException {
         //order.delBouquet(0);
         AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("deleteBouquet.fxml")))));
-
     }
     public void showOrder(ActionEvent e) throws IOException {
         AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("showOrder.fxml")))));
     }
     public void payOrder(ActionEvent e) throws IOException {
-        AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("showOrder.fxml")))));
+        AppMainMenu.getPrimaryStage().setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("payOrder.fxml")))));
     }
-    public void cancelOrder(ActionEvent e) throws IOException {
+    public void cancelOrder(ActionEvent e) {
         this.order=new Order();
     }
-    public void exit(ActionEvent e) throws IOException {
+    public void exit(ActionEvent e) {
         System.exit(0);
+    }
+    public void back(ActionEvent e){
+        AppMainMenu.initRootLayout();
     }
 }
