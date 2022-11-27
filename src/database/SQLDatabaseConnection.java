e1964fa525a442337fbd639d2a5eae6a003ff38f
DarkMlrage <92674008+DarkMlrage@users.noreply.github.com>
2022-11-27 18:56:30 +0200

    With Connection to POSTGRESQL

---
+++ src/database/SQLDatabaseConnection.java
@@ -0,0 +1,22 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLDatabaseConnection {
+    private static final String url = "jdbc:postgresql://localhost/Cursova";
+    private static final String user = "postgres";
+    private static final String password = "1";
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
